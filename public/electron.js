6ad0e5a4d1c0ebf7e6ba3e6e44640a2d27c8e013
Piotr Kasperski <piotr.kasperski94@gmail.com>
2019-08-17 15:16:46 +0200

    devtools work

--- public/electron.js
+++ public/electron.js
@@ -8,12 +8,13 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({width: 900, height: 680});
+    mainWindow = new BrowserWindow({width: 820, height: 700});
     mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
     if (isDev) {
         // Open the DevTools.
-        //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
-        mainWindow.webContents.openDevTools();
+        BrowserWindow.addDevToolsExtension('/home/klonek/.config/BraveSoftware/Brave-Browser/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.0.2_0/');
+        BrowserWindow.addDevToolsExtension('/home/klonek/.config/BraveSoftware/Brave-Browser/Default/Extensions/lmhkpmbekcpmknklioeibfkpmmfibljd/2.17.0_0/');
+        mainWindow.webContents.openDevTools({mode: "bottom");
     }
     mainWindow.on('closed', () => mainWindow = null);
 }
